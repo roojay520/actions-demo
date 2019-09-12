0bdcb1da30aa56a10bf01db625e74947e8dfe578
roojay520 <roojay520@users.noreply.github.com>
2019-09-12 07:14:36 +0000

    Deploying to gh-pages - 07:14:36

---
+++ precache-manifest.8f3fdf7ef6e4a7fbddfc1abd02e1f9b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "364a1a7f9bf237325908556653cfb503",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad71be1c00a0cca6f688",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "0b98c62a8029b26c81fc",
+    "url": "/static/js/2.472f021d.chunk.js"
+  },
+  {
+    "revision": "ad71be1c00a0cca6f688",
+    "url": "/static/js/main.8003ade2.chunk.js"
+  },
+  {
+    "revision": "6a193a9c29f7a6fbcfb4",
+    "url": "/static/js/runtime~main.fc6c27ef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
